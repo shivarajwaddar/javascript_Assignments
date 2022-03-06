3dafc535d8ee58ecd909aa7b1fa5313897d70a22
shivaraj <shivarajwaddar123@gmail.com>
2022-03-06 16:55:31 +0530

    play_with_array_submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+    let even_arr=[];
+    for(let i=0; i<arr.length; i++){
+        if ((arr[i]%2)==0){
+            even_arr.push(arr[i])
+        }    
+    }
+    return even_arr;
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,11 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    let mult_arr=[];
+    for (let i=0; i<arr.length; i++){
+        mult_arr.push(arr[i]*n)
+    }
+    return mult_arr;
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +39,11 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    arr.splice(n,1);
+    return arr
+        
+    
+    
 }
 
 module.exports = {
